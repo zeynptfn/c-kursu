a589c0b6a9004199eb7a4bd302cb0bd33b4a5f6e
zeynptfn <70269084+zeynptfn@users.noreply.github.com>
2021-08-22 13:06:32 +0300

    c#3.hafta

---
+++ GenerictsIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenerictsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> isimler = new MyList<int>();
+
+        }
+    }
+}
